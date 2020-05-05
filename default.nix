a7b7926e976bc29256ed3a3e6aecb6764c76cc20
Thordur Hermannsson <doddi@kott.is>
2020-05-05 00:14:41 +0000

    adding nix build

---
+++ default.nix
@@ -0,0 +1,29 @@
+{ mkDerivation, base, bytestring, directory, exceptions, filepath
+, hpack, imagemagick, lifted-base, QuickCheck, resourcet, stdenv
+, tasty, tasty-hunit, text, transformers, vector
+}:
+mkDerivation {
+  pname = "imagemagick";
+  version = "0.0.6.0";
+  src = ./.;
+  doCheck = false;
+  isLibrary = true;
+  isExecutable = true;
+  buildDepends = [ imagemagick ];
+  configureFlags = [
+    "--extra-include-dirs=${imagemagick.dev}/include/ImageMagick"
+  ];
+  libraryHaskellDepends = [
+    base bytestring filepath resourcet text transformers vector
+  ];
+  libraryPkgconfigDepends = [ imagemagick ];
+  librarySystemDepends = [ imagemagick ];
+  libraryToolDepends = [ hpack ];
+  testHaskellDepends = [
+    base bytestring directory exceptions filepath lifted-base
+    QuickCheck resourcet tasty tasty-hunit text transformers vector
+  ];
+  prePatch = "hpack";
+  description = "bindings to imagemagick library";
+  license = stdenv.lib.licenses.mit;
+}
---
+++ shell.nix
@@ -0,0 +1,41 @@
+{ nixpkgs ? import ./packages.nix {}, compiler ? "default", doBenchmark ? false }:
+
+let
+
+  inherit (nixpkgs) pkgs;
+
+  f = { mkDerivation, base, bytestring, directory, exceptions
+      , filepath, hpack, imagemagick, lifted-base, QuickCheck, resourcet
+      , stdenv, tasty, tasty-hunit, text, transformers, vector
+      }:
+      mkDerivation {
+        pname = "imagemagick";
+        version = "0.0.5.1";
+        src = ./.;
+        isLibrary = true;
+        isExecutable = true;
+        libraryHaskellDepends = [
+          base bytestring filepath resourcet text transformers vector
+        ];
+        libraryPkgconfigDepends = [ pkgs.imagemagickBig ];
+        libraryToolDepends = [ hpack ];
+        testHaskellDepends = [
+          base bytestring directory exceptions filepath lifted-base
+          QuickCheck resourcet tasty tasty-hunit text transformers vector
+        ];
+        prePatch = "hpack";
+        description = "bindings to imagemagick library";
+        license = stdenv.lib.licenses.mit;
+      };
+
+  haskellPackages = if compiler == "default"
+                       then pkgs.haskellPackages
+                       else pkgs.haskell.packages.${compiler};
+
+  variant = if doBenchmark then pkgs.haskell.lib.doBenchmark else pkgs.lib.id;
+
+  drv = variant (haskellPackages.callPackage f {});
+
+in
+
+  if pkgs.lib.inNixShell then drv.env else drv
